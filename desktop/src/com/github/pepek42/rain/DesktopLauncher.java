1ece34b03d46a2e2d9a79964b8cf7bca49c2385d
Pepek42 <8071251+pepek42@users.noreply.github.com>
2023-08-21 23:27:16 +0200

    Initial commit

---
+++ desktop/src/com/github/pepek42/rain/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.github.pepek42.rain;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.github.pepek42.rain.Rain;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Rain");
+		new Lwjgl3Application(new Rain(), config);
+	}
+}
---
+++ html/src/com/github/pepek42/rain/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.github.pepek42.rain.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.github.pepek42.rain.Rain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Rain();
+        }
+}
